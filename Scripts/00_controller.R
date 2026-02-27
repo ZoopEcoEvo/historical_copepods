b18e4c7062f2e49e04f33447b3bf0bc97084b1f6
Matt Sasaki <mcsasaki20@gmail.com>
2026-02-27 10:53:18 -0500

    Adding historical air temperatures and updates to the species occurrence and site data

--- Scripts/00_controller.R
+++ Scripts/00_controller.R
@@ -12,6 +12,34 @@ knit_manuscript = F #Compiles manuscript draft
 ############################
 
 if(process_data == T){
+  
+  # # This section will process the Boston area air temperature record
+  # temp_record = readr::read_csv(list.files(path = "Raw_data/air_temp", 
+  #                                             pattern = "*.csv", 
+  #                                             full.names = TRUE),
+  #                                  skip = 1,
+  #                                  id = "location",
+  #                                  show_col_types = FALSE) %>% 
+  #   janitor::clean_names() %>% 
+  #   select(location, date, mean = tavg_degrees_fahrenheit, max = tmax_degrees_fahrenheit, min = tmin_degrees_fahrenheit) %>%  
+  #   mutate(date = lubridate::as_date(date), 
+  #          location = str_split_fixed(str_remove(location, "Raw_data/air_temp/"), "_", n = 2)[,1],
+  #          mean = (mean - 32) * (5/9), 
+  #          max = (max - 32) * (5/9), 
+  #          min = (min - 32) * (5/9)) %>% 
+  #   group_by(date) %>% 
+  #   summarise(mean = mean(mean, na.rm = T),
+  #             max = mean(max, na.rm = T), 
+  #             min = mean(min, na.rm = T)) %>% 
+  #   mutate(mean_est = (max + min) / 2, 
+  #          diff = mean_est - mean,
+  #          year = year(date))
+  # 
+  # write.csv(temp_record, file = "Output/Output_data/temp_record.csv", row.names = F)
+  
+  sites = read.csv(file = "Raw_data/site_data copy.csv", strip.white = T)
+  species = read.csv(file = "Raw_data/species_occurrence copy.csv", strip.white = T)
+  
   source(file = "Scripts/01_data_processing.R")
 }
 
@@ -19,7 +47,23 @@ if(process_data == T){
 ### Read in the PROCESSED data ###
 ##################################
 
+# stations = readr::read_csv(list.files(path = "Raw_data/air_temp", 
+#                            pattern = "*.csv", 
+#                            full.names = TRUE),
+#                 n_max = 1,
+#                 id = "location",
+#                 col_names = FALSE,
+#                 show_col_types = FALSE)
+
+temp_record = read.csv(file = "Output/Output_data/temp_record.csv") %>% 
+  mutate(date = as_date(date))
+
+hist_occ = read.csv(file = "Output/Output_data/occurrence_database.csv")
+
 if(make_report == T){
+  library(sf)
+  library(tigris)
+  
   render(input = "Output/Reports/report.Rmd", #Input the path to your .Rmd file here
          #output_file = "report", #Name your file here if you want it to have a different name; leave off the .html, .md, etc. - it will add the correct one automatically
          output_format = "all")
--- Scripts/01_data_processing.R
+++ Scripts/01_data_processing.R
@@ -2,4 +2,38 @@
 
 # Data Analysis
 
+### Combining the species occurrence with the site data
+
+select_sites = sites %>% 
+  select(site, region, subregion, salinity, lat, long, confidence) %>% 
+  mutate(sal_num = parse_number(salinity), 
+         salinity = case_when(
+           is.na(sal_num) ~ salinity, 
+           sal_num < 3 ~ "fresh water",
+           sal_num > 3 & sal_num <= 30.16 ~ "brackish", 
+           sal_num > 30.16 ~ "salt water"
+         )) %>% 
+  filter(salinity != "salt water") %>% 
+  select(site, lat, long, confidence, region, salinity)
+
+select_species = species %>% 
+  separate_longer_delim(cols = "sites", delim = ", ") %>% 
+  select("species" = updated_species, "site" = sites)
+
+database = left_join(select_species, select_sites) %>% 
+  filter(species != "")
+
+database %>%  
+  filter(is.na(region))
+# Known species observations missing site info: 
+
+select_sites %>%  
+  filter(!(site %in% unique(select_species$site)))
+# Known site NOT present in the species occurrences 
+# Brackish Ponds on Chappaquiddick Island
+
+write.csv(database, file = "Output/Output_data/occurrence_database.csv", row.names = F)
+
 # Write the data to files in the Output directory
+
+
